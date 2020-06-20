cdb0deb5d37e21f0f786eea0953a6b30c96183a6
Sandhya Patil <sandhyapatil364@gmail.com>
2020-06-20 22:33:11 +0530

    Welcome To Indian Census Analyser

---
+++ IndianCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Indian State Census Analyser");
+        }
+    }
+}
